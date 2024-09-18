5f4e85cdedd92e0ee4add9752067d546fdb69704
aaaaaaaaaaaaaaaaaaaaa <163067846+PalitoDeDente@users.noreply.github.com>
2024-09-18 00:43:12 -0300

    Update Pet.java
    Teoricamente, tá tudo certo aqui, é pouca coisa mesmo

--- Pet.java
+++ Pet.java
@@ -1,16 +1,16 @@
 public class Pet{
-   //Atributos da classe 
-   private String nomePet;
-   private String tipoPet;
-   //metodo contrutor
-   public Pet(String nomePet, String tipoPet){
-      this.nomePet = nomePet;
-      this.tipoPet = tipoPet; 
-   }
-   public String getnomePet(){
-   return nomePet;
-   }
-   public String gettipoPet(){
-   return tipoPet;
-   }
+	//Atributos da classe 
+	private String nomePet;
+	private String tipoPet;
+	//metodo contrutor
+	public Pet(String nomePet, String tipoPet){
+		this.nomePet = nomePet;
+		this.tipoPet = tipoPet; 
+	}
+	public String getnomePet(){
+		return nomePet;
+	}
+	public String gettipoPet(){
+		return tipoPet;
+	}
 }
